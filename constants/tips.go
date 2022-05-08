dbfa299378e0d15356ea1a39e859b6a08905b119
tanzhixuan <tanzhixuan2018@163.com>
2022-05-08 23:01:10 +0800

    fix:login

--- constants/tips.go
+++ constants/tips.go
@@ -2,6 +2,7 @@ package constants
 
 const TIPS_NOT_LOGIN = "未登录"
 const TIPS_TOKEN_NOT_VALID = "token无效"
+const TIPS_TOKEN_VALID_WITH_ERROR = "token有效，但存在其他错误"
 const TIPS_USERNAME_IS_REGISTERED = "用户名已注册"
 const TIPS_USERNAME_OR_PASSWORD_ERROR = "用户名或者密码错误"
 const TIPS_HAS_SAME_DIR = "已有同名文件夹"
--- middlewares/auth.go
+++ middlewares/auth.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"gitee.com/tzxhy/web/constants"
+	"gitee.com/tzxhy/web/models"
 	"gitee.com/tzxhy/web/utils"
 	"github.com/gin-gonic/gin"
 )
@@ -25,6 +26,14 @@ func NeedAuth() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+
+		has := models.GetKey(token)
+		if has == nil {
+			c.JSON(http.StatusOK, utils.ReturnJSON(constants.CODE_TOKEN_NOT_VALID, constants.TIPS_TOKEN_VALID_WITH_ERROR, nil))
+			c.Abort()
+			return
+		}
+
 		// 将当前请求的username信息保存到请求的上下文c上
 		c.Set("username", mc.Username)
 		c.Set("uid", mc.UserId)
